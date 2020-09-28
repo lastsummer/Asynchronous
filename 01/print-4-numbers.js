1e9870ea63a52fbd700c40de8588e44dd96cd9de
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2020-09-28 10:28:02 +0800

    add 01

---
+++ 01/print-4-numbers.js
@@ -0,0 +1,33 @@
+function print1(){
+  const number1 = 1
+  console.log(number1)
+}
+
+function print2(){
+  function getNumber2(){
+    return 2
+  }
+  const number2 = getNumber2()
+  console.log(number2)
+}
+
+function print3(){
+  const fs = require('fs')
+  fs.readFile('./number3.txt', 'utf-8', function(err, number3){
+    console.log(number3)
+  })
+}
+
+function print4(){
+  /*
+  var ms = 3000 + new Date().getTime(); // 需要等待三秒
+  while (new Date() < ms){}
+  */
+  const number4 = 4
+  console.log(number4)
+}
+
+print1()
+print2()
+print3()
+print4()
